94d7d9bac20553d4dd3bb118f3efa46fac22999a
HwangSukHyeon <payback9488@gmail.com>
2024-12-09 12:17:27 +0900

    [feat]: Initialize project setup
    - 프로젝트 생성
    - 코드베이스 환경 구축
    - SnapKit 추가

---
+++ NBC_W7-8_PhoneBook/NBC_W7-8_PhoneBook/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NBC_W7-8_PhoneBook
+//
+//  Created by 황석현 on 12/9/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
