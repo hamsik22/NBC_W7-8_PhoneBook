00986bd598c14b7ac951878b7725588ec5ff6c30
HwangSukHyeon <payback9488@gmail.com>
2024-12-09 14:33:05 +0900

    [file]: Create PhoneBookViewController.swift

---
+++ NBC_W7-8_PhoneBook/NBC_W7-8_PhoneBook/PhoneBookViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhoneBookViewController.swift
+//  NBC_W7-8_PhoneBook
+//
+//  Created by 황석현 on 12/9/24.
+//
+
+import UIKit
+
+class PhoneBookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
