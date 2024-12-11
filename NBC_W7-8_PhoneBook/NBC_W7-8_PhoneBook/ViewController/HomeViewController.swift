b01d55cd806e50aabccb681ac0744c629b74240c
HwangSukHyeon <payback9488@gmail.com>
2024-12-11 17:59:45 +0900

    [Level5]: Complete Essential Feature

--- NBC_W7-8_PhoneBook/NBC_W7-8_PhoneBook/ViewController/HomeViewController.swift
+++ NBC_W7-8_PhoneBook/NBC_W7-8_PhoneBook/ViewController/HomeViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 import SnapKit
 import CoreData
+import Alamofire
 
 class HomeViewController: UIViewController {
+    
+    private let profileImage: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .white
+        image.contentMode = .scaleAspectFit
+        image.layer.cornerRadius = 100
+        image.layer.borderWidth = 1
+        image.layer.borderColor = UIColor.black.cgColor
+        return image
+    }()
     
     let topLabel: UIView = {
         let stackView = UIView()
@@ -38,19 +49,7 @@ class HomeViewController: UIViewController {
         return tableView
     }()
     
-    // 임시 데이터
-    var mockData: [PhoneBookData] = [PhoneBookData(image: UIImage(systemName: "person"),
-                                                   name: "name", phoneNumber: "010-0000-1234"),
-                                     PhoneBookData(image: UIImage(systemName: "person"),
-                                                   name: "name", phoneNumber: "010-0000-0000"),
-                                     PhoneBookData(image: UIImage(systemName: "person"),
-                                                   name: "name", phoneNumber: "010-0000-0000"),
-                                     PhoneBookData(image: UIImage(systemName: "person"),
-                                                   name: "name", phoneNumber: "010-0000-0000"),
-                                     PhoneBookData(image: UIImage(systemName: "person"),
-                                                   name: "name", phoneNumber: "010-0000-0000"),
-                                     PhoneBookData(image: UIImage(systemName: "person"),
-                                                   name: "name", phoneNumber: "010-0000-0000")]
+    var phoneBookData: [PhoneBookData] = []
     
     var container: NSPersistentContainer!
     
@@ -62,11 +61,14 @@ class HomeViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addItemButton)
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         self.container = appDelegate.persistentContainer
-        createCoreData(profile: "URL", name: "Test1", phoneNumber: "010-111-222")
         readCoreData()
-        updateCoreData(oldName: "Test1", newName: "Success")
-        deleteCoreData(name: "Success")
-        deleteCoreData(name: "Name")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        phoneBookData.removeAll()
+        readCoreData()
+        mainTableView.reloadData()
     }
     
     private func setupUI() {
@@ -122,7 +124,7 @@ class HomeViewController: UIViewController {
 // MARK: - Extensions
 extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockData.count
+        return phoneBookData.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -131,16 +133,21 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath) as? ProfileCell else { fatalError() }
-        cell.nameLabel.text = mockData[indexPath.row].name
-        cell.phoneNumberLabel.text = mockData[indexPath.row].phoneNumber
-        //TODO: 이미지 할당 필요
-        
+        cell.nameLabel.text = phoneBookData[indexPath.row].name
+        cell.phoneNumberLabel.text = phoneBookData[indexPath.row].phoneNumber
+        let urlComponents = URLComponents(string: phoneBookData[indexPath.row].image)
+        guard let url = urlComponents?.url else { return UITableViewCell() }
+        AF.request(url).response { respose in
+            if let data = respose.data {
+                cell.profileImage.image = UIImage(data: data)
+            }
+        }
         return cell
     }
     
     // MARK: - CoreData
     
-    // TODO: 데이터 생성
+    /// 데이터 생성
     func createCoreData(profile: String, name: String, phoneNumber: String) {
         guard let entity = NSEntityDescription.entity(forEntityName: PhoneBook.className, in: self.container.viewContext) else { return }
         let newPhoneBook = NSManagedObject(entity: entity, insertInto: self.container.viewContext)
@@ -156,16 +163,18 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             print("Error: ", error)
         }
     }
-    // TODO: 데이터 조회
+    
+    /// 데이터 조회
     func readCoreData() {
         do {
             let phoneBooks = try self.container.viewContext.fetch(PhoneBook.fetchRequest())
             
             for phoneBook in phoneBooks as [NSManagedObject] {
-                if let name = phoneBook.value(forKey: PhoneBook.Key.name) as? String,
+                 if let name = phoneBook.value(forKey: PhoneBook.Key.name) as? String,
                    let phoneNumber = phoneBook.value(forKey: PhoneBook.Key.phoneNumber) as? String,
-                   let profile = phoneBook.value(forKey: PhoneBook.Key.profile) {
+                   let profile = phoneBook.value(forKey: PhoneBook.Key.profile) as? String {
                     print("profile: \(profile), name: \(name), phoneNumber: \(phoneNumber)")
+                    self.phoneBookData.append(PhoneBookData(image: profile, name: name, phoneNumber: phoneNumber))
                 }
             }
         } catch {
@@ -173,7 +182,8 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             print("Error: ", error)
         }
     }
-    // TODO: 데이터 변경
+    
+    ///  데이터 변경
     func updateCoreData(oldName: String, newName: String) {
         let fetchRequest = PhoneBook.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "name == %@", oldName)
@@ -191,7 +201,8 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             print("Error: ", error)
         }
     }
-    // TODO: 데이터 삭제
+    
+    /// 데이터 삭제
     func deleteCoreData(name: String) {
         let fetchRequest = PhoneBook.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "name == %@", name)
@@ -211,7 +222,6 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             print("Error: ", error)
         }
     }
-    
 }
 
 extension HomeViewController: HomeViewControllerDelegate {
